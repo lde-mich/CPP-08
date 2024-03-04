028e23681e401a3f64603f3ef0109bb1161930fa
lde-mich <luca01112003@gmail.com>
2024-03-04 18:15:02 +0100

    upload-mar-4

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -6,11 +6,22 @@
 /*   By: lde-mich <lde-mich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 17:58:31 by lde-mich          #+#    #+#             */
-/*   Updated: 2024/03/01 17:59:00 by lde-mich         ###   ########.fr       */
+/*   Updated: 2024/03/04 18:13:41 by lde-mich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include <iostream>
+#include <vector>
+
+template <typename T>
+bool easyFind(T container, int index)
+{
+	for (size_t i = 0; i < container.size(); i++)
+		if (container[i] == index)
+			return (true);
+
+	return (false);
+}
 
 
