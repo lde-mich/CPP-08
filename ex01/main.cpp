a71d12e856b0e0aceba13e6173d06ecf39a711ec
lde-mich <luca01112003@gmail.com>
2024-03-13 00:15:26 +0100

    upload-mar-13

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lde-mich <lde-mich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 16:06:40 by lde-mich          #+#    #+#             */
-/*   Updated: 2024/03/07 18:19:10 by lde-mich         ###   ########.fr       */
+/*   Updated: 2024/03/13 00:14:47 by lde-mich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,13 @@ int main ()
 
 	try
 	{
-		sp.addNumber(6);
-		sp.addNumber(3);
-		sp.addNumber(17);
-		sp.addNumber(9);
-		sp.addNumber(11);
+		//sp.addNumber(6);
+		//sp.addNumber(3);
+		//sp.addNumber(17);
+		//sp.addNumber(9);
+		//sp.addNumber(1);
 
-		//sp.addMoreNumber();
+		sp.addMoreNumber();
 
 		std::cout << sp.shortestSpan() << std::endl;
 		std::cout << sp.longestSpan() << std::endl;
